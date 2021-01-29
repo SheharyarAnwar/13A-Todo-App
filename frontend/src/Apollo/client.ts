32078309baa47b8ca9453dbdd01e0079d4bed3cf
Sherry <sheharyar28@hotmail.com>
2021-01-29 16:46:13 +0500

    Fixed client not updating on signout

--- frontend/src/Apollo/client.ts
+++ frontend/src/Apollo/client.ts
@@ -9,19 +9,21 @@ import {
   InMemoryCache,
 } from "@apollo/client"
 Amplify.configure(awsconfig)
-const url = awsconfig.aws_appsync_graphqlEndpoint
-
-const region = awsconfig.aws_appsync_region
-const auth = {
-  type: AUTH_TYPE["AMAZON_COGNITO_USER_POOLS"] as "AMAZON_COGNITO_USER_POOLS",
-  jwtToken: async () =>
-    (await Auth.currentSession()).getIdToken().getJwtToken(),
+export const getClient = () => {
+  const url = awsconfig.aws_appsync_graphqlEndpoint
+  const region = awsconfig.aws_appsync_region
+  const auth = {
+    type: AUTH_TYPE["AMAZON_COGNITO_USER_POOLS"] as "AMAZON_COGNITO_USER_POOLS",
+    jwtToken: async () =>
+      (await Auth.currentSession()).getIdToken().getJwtToken(),
+  }
+  console.log(auth)
+  const link = ApolloLink.from([
+    createAuthLink({ url, region, auth }),
+    createHttpLink({ uri: url }),
+  ])
+  return new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+  })
 }
-const link = ApolloLink.from([
-  createAuthLink({ url, region, auth }),
-  createHttpLink({ uri: url }),
-])
-export const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache(),
-})
